caf165b6424a3f091473e2e1f5dc33f98935a546
JeethanTauro <tauroshanjeeth@gmail.com>
2026-03-10 20:27:10 +0530

    wrote store file

---
+++ embedding/store.py
@@ -0,0 +1,13 @@
+#now once u get the embedding store it in the vector store
+import chromadb
+from etl.warehouse import con
+import uuid
+client = chromadb.PersistentClient(path="data/chroma")
+collection = client.get_or_create_collection("mindful_articles")
+
+def add_chunks_to_collection(chunk_with_metadata,embeddings):
+    collection.add(
+        embeddings=[embeddings.tolist()] ,
+        metadatas=[{ "article_id": chunk_with_metadata.get("id"), "source": chunk_with_metadata.get("source"), "title": chunk_with_metadata.get("title"),"author":chunk_with_metadata.get("author"),"url":chunk_with_metadata.get("url"),"chunk_index":chunk_with_metadata.get("chunk_index")}],
+        ids=[str(uuid.uuid4())],
+    )
