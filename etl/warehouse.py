4fead44158d9ce2a7e85e5b48c77727c2fb174c5
JeethanTauro <tauroshanjeeth@gmail.com>
2026-03-11 20:02:27 +0530

    changed data path string

--- etl/warehouse.py
+++ etl/warehouse.py
@@ -3,9 +3,11 @@ the final storage where the cleaned , enriched data that is stored
 this is where the RAG will get the data from
 '''
 import duckdb
+import os
 
 #created a table
-con = duckdb.connect("data/mindful.db")
+DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),"..", "data", "mindful.db")
+con = duckdb.connect(DB_PATH)
 con.execute("CREATE TABLE IF NOT EXISTS articles_warehouse (id VARCHAR,source_id VARCHAR,source VARCHAR,url VARCHAR,title VARCHAR,author VARCHAR,content VARCHAR,word_count INTEGER,reading_time INTEGER, language VARCHAR, tags VARCHAR[],published_at DATETIME,scraped_at DATETIME, processed_at DATETIME,updated_at DATETIME,embedding_id VARCHAR )")
 
 
