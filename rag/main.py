c833cd0be731a18044494cf7f7ccd817780696b8
JeethanTauro <tauroshanjeeth@gmail.com>
2026-03-16 10:14:20 +0530

    refactored main.py in rag for fastapi

--- rag/main.py
+++ rag/main.py
@@ -1,38 +1,31 @@
 #this is the main file that wires all together
-from query_from_user import user_query
-from query_enhancer import enhancer
-from query_embedder import embed_query
+
+from rag.query_enhancer import enhancer
+from rag.query_embedder import embed_query
 from rag.guard import guard
-from similarity_search import search
-from reranker import  rerank
-from context_builder import context_builder
-from chat import llm_chat
-if __name__ == '__main__':
-    memory = []
-    while True:
-        query = user_query() #taking the user query
+from rag.similarity_search import search
+from rag.reranker import  rerank
+from rag.context_builder import context_builder
+from rag.chat import llm_chat
 
-        safe, msg = guard(query)
-        if not safe:
-            print(f"Mindful: {msg}")
-            continue
 
-        enhanced_query= enhancer(query,memory) #enchancing it according to memory
+def run(query,memory):
+    if memory is None:
+        memory = []
 
-        embeddings = embed_query(enhanced_query) #get embeddings of the query
+    safe, msg = guard(query)
+    if not safe:
+        return {"answer": msg, "safe": False}
 
-        chunks,metadata = search(embeddings=embeddings) #from the query get the chunks and metadata
+    enhanced_query = enhancer(query, memory)
+    embeddings = embed_query(enhanced_query)
+    chunks, metadata = search(embeddings=embeddings)
+    chunks, metadata = rerank(enhanced_query, chunks[0], metadata[0])
+    context = context_builder(chunks, metadata, enhanced_query)
+    output = llm_chat(context, memory)
 
-        chunks,metadata = rerank(enhanced_query,chunks[0],metadata[0])#reranking the chunks
+    return {"answer": output, "safe": True}
 
-        context = context_builder(chunks,metadata,enhanced_query) #building the context for the chatbot
-
-        output = llm_chat(context,memory) #output after prompt
-
-        #building the memory
-        memory.append({"role": "user", "content": query})
-        memory.append({"role": "assistant", "content": output})
-        print(output)
 
 
 
