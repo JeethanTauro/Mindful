8a0ebbd8d709dafc55a0a60b3ab67348e79875f3
JeethanTauro <tauroshanjeeth@gmail.com>
2026-03-16 20:07:06 +0530

    commented a line

--- etl/warehouse.py
+++ etl/warehouse.py
@@ -8,7 +8,7 @@ import os
 #created a table
 DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),"..", "data", "mindful.db")
 con = duckdb.connect(DB_PATH)
-con.execute("CREATE TABLE IF NOT EXISTS articles_warehouse (id VARCHAR,source_id VARCHAR,source VARCHAR,url VARCHAR,title VARCHAR,author VARCHAR,content VARCHAR,word_count INTEGER,reading_time INTEGER, language VARCHAR, tags VARCHAR[],published_at DATETIME,scraped_at DATETIME, processed_at DATETIME,updated_at DATETIME,embedding_id VARCHAR )")
+# con.execute("CREATE TABLE IF NOT EXISTS articles_warehouse (id VARCHAR,source_id VARCHAR,source VARCHAR,url VARCHAR,title VARCHAR,author VARCHAR,content VARCHAR,word_count INTEGER,reading_time INTEGER, language VARCHAR, tags VARCHAR[],published_at DATETIME,scraped_at DATETIME, processed_at DATETIME,updated_at DATETIME,embedding_id VARCHAR )")
 
 
 #if we are doing stream processing one article at a time
